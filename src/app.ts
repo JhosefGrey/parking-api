5209b83395a6bc976b4afa5f4bc31d2349c194e4
Jhosef <jhosefgrey@gmail.com>
2024-05-16 09:25:24 -0600

    mongo connection

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import { envs } from "./config/envs";
+import { MongoDatabase } from "./data/mongo";
 import { Server } from "./presentacion/server";
 
 (async () => {
     main();
 })();
 
-function main() {
+async function main() {
+
+    await MongoDatabase.connect({ mongoUrl: envs.MONGO_URL, dbName: envs.MONGO_DB_NAME })
+
     const server = new Server({
         port: envs.PORT || 3000,
         secret: envs.SECRET || '7156938b-eeb6-4988-8e0a-afb41c7c665e'
--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,4 +4,8 @@ import { get } from 'env-var';
 export const envs = {
     PORT: get('PORT').required().asPortNumber(),
     SECRET: get('SECRET').required().asString(),
+    MONGO_URL: get('MONGO_URL').required().asString(),
+    MONGO_DB_NAME: get('MONGO_DB_NAME').required().asString(),
+    MONGO_USER: get('MONGO_USER').required().asString(),
+    MONGO_PWD: get('MONGO_PWD').required().asString()
 }
