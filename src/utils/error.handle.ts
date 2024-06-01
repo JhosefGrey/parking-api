8cbf4c8ccd1e18352a5fd2bd0e9496df94bbe9ae
Jhosef <jhosefgrey@gmail.com>
2024-06-01 13:03:02 -0600

    error

--- src/utils/error.handle.ts
+++ src/utils/error.handle.ts
@@ -1,7 +1,7 @@
 import { Response } from "express";
 
 const handleHttp = (res: Response, error: string, errorRaw?: any ) => {
-    res.status(500);
+    res.status(400);
     res.send({ error });
 }
 
